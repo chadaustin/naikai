c1117029b6a499a599a07a9aa63da21c482b0052
aegis <aegis@ed5ae262-7be1-47e3-824f-6ab8bb33c1ce>
2001-09-05 09:45:12 +0000

    build system compiles XPT and C++ header files now
    git-svn-id: file:///desktop/SourceForge%20Backup/naikai/svnrepos/trunk@7 ed5ae262-7be1-47e3-824f-6ab8bb33c1ce

--- naikai/build.py
+++ naikai/build.py
@@ -10,6 +10,9 @@ import shutil
 MODULES = [ "base", "windowing" ]
 build_dir = ""
 current_module = ""
+stage = 0  # stage 0 = export IDL
+           # stage 1 = compile IDL
+           # stage 2 = ??
 
 
 def makedirs(p):
@@ -17,26 +20,45 @@ def makedirs(p):
         os.makedirs(p)
 
 
+def should_build(i, o):
+    try:
+        itime = os.stat(i)[stat.ST_MTIME]
+        otime = os.stat(o)[stat.ST_MTIME]
+        return itime > otime
+    except OSError:
+        return 1
+
+    return 0
+
+
 # called by module.py
 def process_idl(list):
 
+    XPIDL_INCLUDES = "-I F:/mozilla/dist/idl -I " + path.join(build_dir, "idl")
+
     for i in list:
 
-        input  = path.join(current_module, "idl", i)
-        output = path.join(build_dir, "idl", i)
+        input = path.join(current_module, "idl", i)
+        output_idl = path.join(build_dir, "idl", i)
+        output_xpt = path.join(build_dir, "xpt", i)
+        output_h   = path.join(build_dir, "include", i)
 
-        should_build = 0
+        # remove extension if we must
+        if output_xpt[-4:] == ".idl":
+            output_xpt = output_xpt[:-4]
+        if output_h[-4:] == ".idl":
+            output_h = output_h[:-4]
 
-        try:
-            itime = os.stat(input)[stat.ST_MTIME]
-            otime = os.stat(output)[stat.ST_MTIME]
-            should_build = itime > otime
-        except OSError:
-            should_build = 1
+        if stage == 0:  # export IDL
+            if should_build(input, output_idl):
+                shutil.copyfile(input, output)
 
-        if should_build:
-            print i
-            shutil.copyfile(input, output)
+        if stage == 1:  # compile IDL
+            if should_build(input, output_xpt):
+                os.system("xpidl " + XPIDL_INCLUDES + \
+                          " -m typelib -o " + output_xpt + " " + input);
+                os.system("xpidl " + XPIDL_INCLUDES + \
+                          " -m header -o " + output_h + " " + input);
 
 
 def build_module(module):
@@ -49,7 +71,7 @@ def build_module(module):
 
 def main():
 
-    global build_dir
+    global build_dir, stage
 
     # figure out if we should parse a command file
     # XXX todo
@@ -72,6 +94,10 @@ def main():
     makedirs(path.join(build_dir, "bin"))
 
     # compile each module
+    stage = 0
+    for i in MODULES:
+        build_module(i)
+    stage = 1
     for i in MODULES:
         build_module(i)
 
